f20e13fde152ac03dc80a774bb84bdafa571d796
OthmaneGhandi <othmane.ghandi@gmail.com>
2016-02-07 18:45:43 +0000

    modif size 16

--- addons/openeducat_erp/op_batch/op_batch.py
+++ addons/openeducat_erp/op_batch/op_batch.py
@@ -25,7 +25,7 @@ from openerp import models, fields
 class OpBatch(models.Model):
     _name = 'op.batch'
 
-    code = fields.Char('Code', size=8, required=True)
+    code = fields.Char('Code', size=16, required=True)
     name = fields.Char('Name', size=32, required=True)
     start_date = fields.Date('Start Date', required=True)
     end_date = fields.Date('End Date', required=True)
